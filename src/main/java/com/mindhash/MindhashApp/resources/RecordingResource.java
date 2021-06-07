7b65464326fc7e27bf297b1625e5be9b571367b3
s2509148 <t.p.runhaar@student.utwente.nl>
2021-06-07 16:20:25 +0000

    Revert "Added precipitation per hour line chart, with restful calls to weather api"
    This reverts commit 6cd1b3689d597e94f44357a4c3b51119993d3377

--- src/main/java/com/mindhash/MindhashApp/resources/RecordingResource.java
+++ src/main/java/com/mindhash/MindhashApp/resources/RecordingResource.java
@@ -4,7 +4,6 @@ import com.mindhash.MindhashApp.dao.RecordingDao;
 import com.mindhash.MindhashApp.model.Recording;
 
 import javax.ws.rs.GET;
-import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -29,18 +28,6 @@ public class RecordingResource {
     @GET
     @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
     public Recording getRecordingDetails() {
-        Recording recording = RecordingDao.instance.getModel().get(id);
-        if (recording == null) {
-            throw new RuntimeException("Recording with id " + id + "not found");
-        }
-        return recording;
-    }
-
-    @Path("/times")
-    @GET
-    @Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
-    public Recording getRecordingTimes() {
-        // to be implement get start time and end time as json
         Recording recording = RecordingDao.instance.getModel().get(id);
         if (recording == null) {
             throw new RuntimeException("Recording with id " + id + "not found");
--- src/main/webapp/js/recording.js
+++ src/main/webapp/js/recording.js
@@ -1,6 +1,5 @@
 let pChart = null,
 	bChart = null;
-	rainChart = null;
 const btn = document.querySelector(".logo");
 btn.addEventListener("click", function () {
 	if (currentTheme == "dark") {
@@ -47,8 +46,7 @@ xmlhttp.onreadystatechange = function() {
 			recording = JSON.parse(response),
 			ctx = document.querySelector('#pie-chart').getContext('2d'),
 			ctx2 = document.querySelector('#bar-chart').getContext('2d');
-			ctx3 = document.querySelector('#rainchart').getContext('2d');
-
+		
 		var htmlStr = "<div>Date: " + recording.date + "</div>" + 
 					"<div>Total: " + recording.totalObjects + "</div>" +
 					"<div>Pedestrians: " + recording.totalPedestrians + "</div>" + 
@@ -141,68 +139,8 @@ xmlhttp.onreadystatechange = function() {
 				responsive: true,
 				maintainAspectRatio: false
 			}
-		})
-
-
-		// get closest station
-		let xmlhttp2 = new XMLHttpRequest();
-		xmlhttp2.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				var response = this.responseText
-				jsonstations = JSON.parse(response)
-				for (i = 0; i < 2; i++) {
-					if (jsonstations.data[i].active === true) {
-						break;
-					}
-				}
-				let closeststation = jsonstations.data[i].name.en
-				let closestactivestationid = jsonstations.data[i].id
-
-				//get weather info from that station
-				let xmlhttp3 = new XMLHttpRequest();
-				xmlhttp3.onreadystatechange = function() {
-					if (this.readyState == 4 && this.status == 200) {
-						var response = this.responseText
-						hourlyweather = JSON.parse(response)
-						console.log(hourlyweather.data[17].prcp)
-						var raindata = ""
-						for (i = 0; i < 24; i++) {
-							raindata += hourlyweather.data[i]
-						}
-
-						rainChart = new Chart(ctx3, {
-							type: 'line',
-							data: {
-								labels: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
-								datasets: [{
-									data: [hourlyweather.data[0].prcp,hourlyweather.data[1].prcp,hourlyweather.data[2].prcp,hourlyweather.data[3].prcp,hourlyweather.data[4].prcp,hourlyweather.data[5].prcp,hourlyweather.data[6].prcp,hourlyweather.data[7].prcp,hourlyweather.data[8].prcp,hourlyweather.data[9].prcp,hourlyweather.data[10].prcp,hourlyweather.data[11].prcp,hourlyweather.data[12].prcp,hourlyweather.data[13].prcp,hourlyweather.data[14].prcp,hourlyweather.data[15].prcp,hourlyweather.data[16].prcp,hourlyweather.data[17].prcp,hourlyweather.data[18].prcp,hourlyweather.data[19].prcp,hourlyweather.data[20].prcp,hourlyweather.data[21].prcp,hourlyweather.data[22].prcp,hourlyweather.data[23].prcp],
-									label: "Precipitation per hour (in mm) given by source "+ closeststation,
-									borderColor: "#3e95cd",
-									fill: false
-								}
-								]
-							},
-							options: {
-								title: {
-									display: true,
-									text: 'Precipitation per hour (in mm) given by source '+ closeststation
-								}
-							}
-						});
-
-					}
-				}
-				xmlhttp3.open("GET", "https://api.meteostat.net/v2/stations/hourly?station="+ closestactivestationid +"&start="+ recording.date +"&end="+ recording.date +"&tz=CEST", true);
-				xmlhttp3.setRequestHeader("x-api-key", "qrTVaDSZR5djogSYK67hIjqFBy1avCTk");
-				xmlhttp3.send();
-
-
-			}
-		}
-		xmlhttp2.open("GET", "https://api.meteostat.net/v2/stations/nearby?lat="+ recording.latitude +"&lon="+ recording.longitude + "&limit=3", true);
-		xmlhttp2.setRequestHeader("x-api-key", "qrTVaDSZR5djogSYK67hIjqFBy1avCTk");
-		xmlhttp2.send();
-		;
+		});
+		
 	}
 }
 xmlhttp.open("GET", "/MindhashApp/rest/recordings/" + id, true);
