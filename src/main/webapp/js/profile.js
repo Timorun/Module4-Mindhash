24701151fcb91870008950f83ccdbcc8dba1511f
Timothy Runhaar <t.p.runhaar@student.utwente.nl>
2021-06-25 13:21:00 +0200

    Profile(admin) page, Updated upload form action. drop-down list of recordingids to give access to

--- src/main/webapp/js/profile.js
+++ src/main/webapp/js/profile.js
@@ -27,44 +27,67 @@ xmlhttp.onreadystatechange = function() {
         document.querySelector("#general-tit").insertAdjacentHTML("afterend", profileStr);
 
         if (user.isadmin) {
+            //add adminfeatures html
             document.getElementById('adminFeatures').innerHTML += "<h1>Admin Features</h1>\n" +
                 "            <div class=\"section-container\">\n" +
-                "                <section class=\"form-wrapper\" <form action=\"uploadServlet\" method=\"post\" enctype=\"multipart/form-data\" onsubmit=\"return validate()\">\n" +
+                "                <section class=\"form-wrapper\" <form action=\"/mindhash/rest/upload/json\" method=\"post\" enctype=\"multipart/form-data\" onsubmit=\"return validate()\">\n" +
                 "                        <label for=\"latitude\">Latitude:</label>\n" +
                 "                        <input id=\"latitude\" name=\"latitude\" type=\"text\" placeholder=\"(e.g. 72.832...)\" class=\"form-input-field\" />\n" +
-                "\n" +
                 "                        <label for=\"longitude\">Longitude:</label>\n" +
                 "                        <input id=\"longitude\" name =\"longitude\" type=\"text\" placeholder=\"(e.g. 25.093...)\" class=\"form-input-field\" />\n" +
-                "\n" +
                 "                        <label for=\"framerate\">Framerate:</label>\n" +
                 "                        <input type=\"text\" id=\"framerate\" name =\"framerate\" placeholder=\"(e.g. 60)\" name=\"framerate\" class=\"form-input-field\">\n" +
-                "\n" +
                 "                        <label for=\"resolution\">Resolution:</label>\n" +
                 "                        <input type=\"text\" id=\"resolution\" name =\"resolution\" placeholder=\"(e.g. 1280x800)\" name=\"resolution\" class=\"form-input-field\">\n" +
-                "\n" +
                 "                        <label for=\"file\">JSON of the recording:</label>\n" +
                 "                        <input type=\"file\" id=\"file\" name = \"file\"><br>\n" +
                 "                        <br>\n" +
-                "\n" +
                 "                        <input type=\"submit\" value=\"Upload\" class=\"form-submit-btn\">\n" +
                 "                        <div id=\"err-msg\" class=\"err-msg hide\"></div>\n" +
-                "                    </form></section></div>" +
-                "<div class=\"section-container\">\n" +
-                "                <section class=\"form-wrapper\" id=\"accessForm\">\n" +
-                "                    <label> Grant access to a recording:</label>\n" +
-                "                    <input type=\"text\" class=\"form-input-field\" placeholder=\"user@mail.com\"></input>\n" +
-                "                    <input type=\"integral\" class=\"form-input-field\" placeholder=\"(e.g. 1)\"></input>\n" +
-                "                    <input id=\"grant\" type=\"submit\" value=\"Grant Access\" class=\"form-submit-btn\" />\n" +
-                "                </section>\n" +
-                "            </div>"
-
+                "                    </form></section></div>"
             document.getElementById('title').innerHTML = "Admin Page"
+
+            //get list of recordingids
+            if (token != null) {
+                let xmlhttp2 = new XMLHttpRequest();
+                xmlhttp2.onreadystatechange = function() {
+                    if (this.readyState == 4 && this.status == 200) {
+                        var response = this.responseText,
+                            recordings = JSON.parse(response),
+                            htmlStr = "<div class=\"section-container\">\n" +
+                                "                <section class=\"form-wrapper\" id=\"accessForm\">\n" +
+                                "                    <label> Grant access to a recording:</label>\n" +
+                                "                    <input type=\"text\" class=\"form-input-field\" placeholder=\"user@mail.com\"></input>\n" +
+                                "<label for=\"recordings\">Choose a recording:</label>\n" +
+                                "<select name=\"cars\" id=\"cars\">";
+                        for(var recording of recordings) {
+                            var id = recording.recordingID;
+                            htmlStr += "<option value="+id+">"+id+"</option>"
+                        }
+                        htmlStr += "</select><button  id=\"grant\" class=\"form-submit-btn\" onclick=\"grantAccess()\">Grant Access</button>" +
+                            "                </section>\n"+
+                            "            </div>"
+                        document.getElementById('adminFeatures').innerHTML += htmlStr;
+                    } else if (this.readyState == 4 && this.status == 511) {
+                        location.href = "login.html";
+                    }
+                }
+                xmlhttp2.open("GET", "/mindhash/rest/recordings", true);
+                xmlhttp2.setRequestHeader("Accept", "application/json");
+                xmlhttp2.setRequestHeader("Authorization", token);
+                xmlhttp2.send();
+            } else {
+                sessionStorage.removeItem("sessionToken");
+                location.href = "login.html";
+            }
         }
-
-
     } else if (this.readyState == 4 && this.status == 511) {
         location.href = "login.html";
     }
 }
 xmlhttp.open("GET", "/mindhash/rest/user/"+token, true);
 xmlhttp.send();
+
+function grantAccess() {
+    console.log("buttonclicked")
+}
