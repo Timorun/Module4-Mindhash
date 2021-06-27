577351ae1b9ef6e9b31e3baaa62a18ac032535d9
Xianzhi Wu <x.wu-5@student.utwente.nl>
2021-06-27 17:11:25 +0200

    rename class

--- src/main/java/com/mindhash/MindhashApp/resources/GrantAccessResource.java
+++ src/main/java/com/mindhash/MindhashApp/resources/GrantAccessResource.java
@@ -14,7 +14,7 @@ import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 
 @Path("/access")
-public class grantAccessResource {
+public class GrantAccessResource {
 
     @POST
     @Path("/{email}/{recordingid}")
